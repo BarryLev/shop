19868507fe91c0c9f077ae80cd92a3d6c0521375
BarryLev <Yaroslavik123@i.ua>
2023-01-21 08:48:27 +0200

    Revert "a"
    This reverts commit 124e26b4c269d4031eedc7af24ae8ee5e05edfe2.

--- db/migrate/20230121061101_drop_relations_to_user.rb
+++
@@ -1,7 +0,0 @@
-class DropRelationsToUser < ActiveRecord::Migration[7.0]
-  def change
-    remove_reference :carts, :user, null: false, foreign_key: true
-    remove_reference :addresses, :user, null: true, foreign_key: true
-    remove_reference :orders, :user, null: true, foreign_key: true
-  end
-end
