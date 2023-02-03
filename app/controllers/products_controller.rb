be54a542c98cce1c5ef1c4e56dcb8bc608b3a9ca
BarryLev <Yaroslavik123@i.ua>
2023-02-03 06:31:54 +0200

    Added orders + refactor

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,29 @@
 class ProductsController < ApplicationController
+  def index
+    @products = get_products
+    @categories = collection_category
+  end
+  
   def show
-    @product = Product.find_by(id: params[:id])
+    @product = resource
+  end
+
+  private
+
+  def get_products
+    return collection_product.with_category(params[:id]) if params[:id]
+    collection_product
+  end
+
+  def collection_product
+    Product.sorted_by_position
+  end
+
+  def collection_category
+    Category.sorted_by_position
+  end
+
+  def resource
+    Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,17 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  root 'pages#home'
-  get 'page/:id', to:'pages#home', as: :get_products_from_category
-  # get 'products/:position', to:'products#show', as: :get_product
+  resources :orders, except: :create
+  post 'order/', to:'orders#create', as: :create_order
+
+  root 'products#index'
+
+  get 'products/:id', to:'products#index', as: :get_products_from_category
+
   get 'cart/', to:'carts#show', as: :get_cart
 
   get 'product/:id', to:'products#show', as: :get_product
   
-  get 'order/', to:'orders#home', as: :home_order
-  # post 'order/', to:'orders#new', as: :new_order
-
   post 'cart/:id', to:'carts#create', as: :add_product
   delete 'cart/:id', to:'carts#destroy', as: :destroy_cart_product
 end
